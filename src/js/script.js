c63551811cef0d80548a7a227b3fd77e49081b74
paulovictor001 <paulo20victor06@gmail.com>
2025-08-07 11:50:00 -0300

    feat:ajustes concluidos

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,9 @@
-const myModal = document.getElementById('Modal')
-const myInput = document.getElementById('Input')
+// const myModal = document.getElementById('Modal')
+// const myInput = document.getElementById('Input')
 
-function activeModal(){
-    myModal.addEventListener('shown.bs.modal', () => {
-      myInput.focus()
-    })
+// function activeModal(){
+//     myModal.addEventListener('shown.bs.modal', () => {
+//       myInput.focus()
+//     })
 
-}
+// }
